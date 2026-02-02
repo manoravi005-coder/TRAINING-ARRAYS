3f44f6ae3dc5c9c2f407d5459917cd93fd6ee2c3
Manobala.R <manoravi005@gmail.com>
2026-02-02 14:43:21 +0530

    Add files via upload

---
+++ row with minimum sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int row,col,i,j,sum;
+	scanf("%d%d", &row, &col);
+	int arr[row][col];
+	for(i = 0; i< row; i++){
+	    for(j = 0; j< col; j++){
+	        scanf("%d", &arr[i][j]);
+	    }
+	}
+	
+	for(i = 0; i<row; i++){
+	        int min = arr[0][i];
+	        for(j =1; j<col; j++){
+	            sum+=arr[j][i];
+	            if(sum<min){
+	                min = arr[j][i];
+	            }
+	    }printf("%d\n", min);
+	
+    }   
+    return 0;
+}    
